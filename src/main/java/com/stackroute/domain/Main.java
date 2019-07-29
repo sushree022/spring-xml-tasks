c30f84600922178cafd44edd936c76340e5b80ee
sushree mishra <sushree022@gmail.com>
2019-07-29 18:02:24 +0530

    ReadMe file updated

--- src/main/java/com/stackroute/domain/Main.java
+++ src/main/java/com/stackroute/domain/Main.java
@@ -4,21 +4,14 @@ import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import com.stackroute.domain.Actor;
-import com.stackroute.domain.Movie;
-import org.springframework.core.io.ClassPathResource;
 
 public class Main {
     public static void main(String[] args) {
 //        Getting Movie bean Using ApplicationContext
 //        creating container
-        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
         Movie movie1 = (Movie) context.getBean("movie1");
         System.out.println(movie1.getActor());
-//        beanlife cycle demo bean
-        ConfigurableApplicationContext context2 = new ClassPathXmlApplicationContext("beans.xml");
-        BeanLifecycleDemoBean bean = (BeanLifecycleDemoBean) context2.getBean("life-cycle-bean");
-//        BeanPostProcessorDemoBean
-        BeanPostProcessor postProcessorBean= (BeanPostProcessor) context2.getBean("post-processor-bean");
+        context.close();
     }
 }
